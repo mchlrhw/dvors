a1a81a03d2668d0dce8e1d4a1bfc1ce6815a170d
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2020-05-15 18:03:20 +0100

    Move typing test code into module (properly this time)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod keymap;
 mod metrics;
+mod typingtest;
 mod word;
 
 use std::{
     collections::{HashSet, VecDeque},
-    convert::From,
     fmt::{self, Display, Formatter},
     io::{stdout, Write},
-    time::{Duration, SystemTime},
 };
 
 use crossterm::{
@@ -26,131 +25,7 @@ use tui::{
     Terminal,
 };
 
-use metrics::Metric;
-use word::{FinishedWord, Word};
-
-struct TestResults<'a>(Vec<FinishedWord<'a>>);
-
-impl TestResults<'_> {
-    fn word_cnt(&self) -> usize {
-        self.0.len()
-    }
-
-    fn char_cnt(&self) -> usize {
-        self.0
-            .iter()
-            .fold(0, |acc, word| acc + word.len_inc_delim())
-    }
-
-    fn duration_secs(&self) -> f64 {
-        let duration = self.0.iter().fold(Duration::default(), |acc, word| {
-            acc + word
-                .metrics()
-                .iter()
-                .fold(Duration::default(), |acc, metric| match metric {
-                    Metric::Delimiter { duration, .. }
-                    | Metric::Match { duration, .. }
-                    | Metric::Typo { duration, .. } => acc + *duration,
-                })
-        });
-
-        duration.as_secs_f64()
-    }
-
-    fn wpm_avg(&self) -> f64 {
-        let word_cnt = self.char_cnt() as f64 / 5.0;
-
-        if !self.0.is_empty() {
-            word_cnt / (self.duration_secs() / 60.0)
-        } else {
-            0.0
-        }
-    }
-
-    fn typo_cnt(&self) -> usize {
-        self.0.iter().fold(0, |acc, word| {
-            acc + word.metrics().iter().fold(0, |acc, metric| {
-                if let Metric::Typo { .. } = metric {
-                    acc + 1
-                } else {
-                    acc
-                }
-            })
-        })
-    }
-}
-
-#[throws]
-fn typing_test<'a, 'b, B: tui::backend::Backend>(
-    terminal: &'b mut Terminal<B>,
-    mut test_words: VecDeque<&'a str>,
-) -> TestResults<'a> {
-    let mut test_word = Word::from(test_words.pop_front().unwrap());
-    let mut typed = String::new();
-    let mut finished_words = vec![];
-
-    let mut start_char = SystemTime::now();
-
-    loop {
-        terminal.draw(|mut frame| {
-            let size = frame.size();
-
-            let remaining_words = test_words
-                .iter()
-                .map(|s| format!(" {}", s))
-                .collect::<String>()
-                .chars()
-                .skip(test_word.overflow())
-                .collect::<String>();
-
-            let mut text = vec![Text::styled(&typed, Style::default().fg(Color::DarkGray))];
-            text.extend_from_slice(&test_word.styled_text());
-            text.push(Text::styled(
-                remaining_words,
-                Style::default().fg(Color::DarkGray),
-            ));
-
-            let block = Block::default()
-                .borders(Borders::ALL)
-                .border_style(Style::default().fg(Color::DarkGray));
-            let paragraph = Paragraph::new(text.iter()).block(block).wrap(true);
-            frame.render_widget(paragraph, size);
-        })?;
-
-        if let Event::Key(event) = read()? {
-            if event.code == KeyCode::Esc {
-                break;
-            }
-            let c = keymap::qwerty_to_dvorak(event.code);
-            match c {
-                KeyCode::Backspace => test_word.remove_char(),
-                KeyCode::Char(c) => {
-                    if c == ' ' && test_word.is_complete() {
-                        typed.push_str(test_word.as_str());
-                        typed.push(' ');
-
-                        let finished_word = test_word.finalise(c, start_char.elapsed()?);
-                        finished_words.push(finished_word);
-
-                        test_word = match test_words.pop_front() {
-                            Some(word) => {
-                                start_char = SystemTime::now();
-                                word.into()
-                            }
-                            None => break,
-                        };
-                    } else {
-                        test_word.add_char(c, start_char.elapsed()?);
-                        start_char = SystemTime::now();
-                    }
-                }
-                _ => {}
-            }
-        }
-    }
-
-    TestResults(finished_words)
-}
+use typingtest::typing_test;
 
 fn get_test_words<'a>(
     word_list: &[&'a str],
--- src/typingtest.rs
+++ src/typingtest.rs
@@ -1,15 +1,37 @@
-use crate::Error;
+use std::{
+    collections::VecDeque,
+    time::{Duration, SystemTime},
+};
 
-struct TestResults<'a>(Vec<FinishedWord<'a>>);
+use crossterm::event::{read, Event, KeyCode};
+use fehler::throws;
+use tui::{
+    style::{Color, Style},
+    widgets::{Block, Borders, Paragraph, Text},
+    Terminal,
+};
+
+use crate::{
+    keymap,
+    metrics::Metric,
+    word::{FinishedWord, Word},
+    Error,
+};
+
+pub struct TestResults<'a>(Vec<FinishedWord<'a>>);
 
 impl TestResults<'_> {
-    fn wpm_avg(&self) -> f64 {
-        let char_cnt = self
-            .0
-            .iter()
-            .fold(0, |acc, word| acc + word.len_inc_delim());
-        let word_cnt = char_cnt as f64 / 5.0;
+    pub fn word_cnt(&self) -> usize {
+        self.0.len()
+    }
 
+    pub fn char_cnt(&self) -> usize {
+        self.0
+            .iter()
+            .fold(0, |acc, word| acc + word.len_inc_delim())
+    }
+
+    pub fn duration_secs(&self) -> f64 {
         let duration = self.0.iter().fold(Duration::default(), |acc, word| {
             acc + word
                 .metrics()
@@ -21,14 +43,20 @@ impl TestResults<'_> {
                 })
         });
 
+        duration.as_secs_f64()
+    }
+
+    pub fn wpm_avg(&self) -> f64 {
+        let word_cnt = self.char_cnt() as f64 / 5.0;
+
         if !self.0.is_empty() {
-            word_cnt / (duration.as_secs_f64() / 60.0)
+            word_cnt / (self.duration_secs() / 60.0)
         } else {
             0.0
         }
     }
 
-    fn typo_cnt(&self) -> usize {
+    pub fn typo_cnt(&self) -> usize {
         self.0.iter().fold(0, |acc, word| {
             acc + word.metrics().iter().fold(0, |acc, metric| {
                 if let Metric::Typo { .. } = metric {
@@ -42,7 +70,10 @@ impl TestResults<'_> {
 }
 
 #[throws]
-fn typing_test<'a>(mut test_words: VecDeque<&'a str>) -> TestResults {
+pub(crate) fn typing_test<'a, 'b, B: tui::backend::Backend>(
+    terminal: &'b mut Terminal<B>,
+    mut test_words: VecDeque<&'a str>,
+) -> TestResults<'a> {
     let mut test_word = Word::from(test_words.pop_front().unwrap());
     let mut typed = String::new();
     let mut finished_words = vec![];
@@ -50,72 +81,62 @@ fn typing_test<'a>(mut test_words: VecDeque<&'a str>) -> TestResults {
     let mut start_char = SystemTime::now();
 
     loop {
-        if poll(Duration::from_millis(100))? {
-            if let Event::Key(event) = read()? {
-                if event.code == KeyCode::Esc {
-                    break;
-                }
-                let c = keymap::qwerty_to_dvorak(event.code);
-                match c {
-                    KeyCode::Backspace => test_word.remove_char(),
-                    KeyCode::Char(c) => {
-                        if c == ' ' && test_word.is_complete() {
-                            typed.push_str(test_word.as_str());
-                            typed.push(' ');
+        terminal.draw(|mut frame| {
+            let size = frame.size();
 
-                            let finished_word = test_word.finalise(c, start_char.elapsed()?);
-                            finished_words.push(finished_word);
+            let remaining_words = test_words
+                .iter()
+                .map(|s| format!(" {}", s))
+                .collect::<String>()
+                .chars()
+                .skip(test_word.overflow())
+                .collect::<String>();
 
-                            test_word = match test_words.pop_front() {
-                                Some(word) => {
-                                    start_char = SystemTime::now();
-                                    word.into()
-                                }
-                                None => break,
-                            };
-                        } else {
-                            test_word.add_char(c, start_char.elapsed()?);
-                            start_char = SystemTime::now();
-                        }
+            let mut text = vec![Text::styled(&typed, Style::default().fg(Color::DarkGray))];
+            text.extend_from_slice(&test_word.styled_text());
+            text.push(Text::styled(
+                remaining_words,
+                Style::default().fg(Color::DarkGray),
+            ));
+
+            let block = Block::default()
+                .borders(Borders::ALL)
+                .border_style(Style::default().fg(Color::DarkGray));
+            let paragraph = Paragraph::new(text.iter()).block(block).wrap(true);
+            frame.render_widget(paragraph, size);
+        })?;
+
+        if let Event::Key(event) = read()? {
+            if event.code == KeyCode::Esc {
+                break;
+            }
+            let c = keymap::qwerty_to_dvorak(event.code);
+            match c {
+                KeyCode::Backspace => test_word.remove_char(),
+                KeyCode::Char(c) => {
+                    if c == ' ' && test_word.is_complete() {
+                        typed.push_str(test_word.as_str());
+                        typed.push(' ');
+
+                        let finished_word = test_word.finalise(c, start_char.elapsed()?);
+                        finished_words.push(finished_word);
+
+                        test_word = match test_words.pop_front() {
+                            Some(word) => {
+                                start_char = SystemTime::now();
+                                word.into()
+                            }
+                            None => break,
+                        };
+                    } else {
+                        test_word.add_char(c, start_char.elapsed()?);
+                        start_char = SystemTime::now();
                     }
-                    _ => {}
                 }
+                _ => {}
             }
         }
-
-        let remaining_words = test_words
-            .iter()
-            .map(|s| format!(" {}", s))
-            .collect::<String>();
-
-        execute!(
-            stdout(),
-            Clear(ClearType::All),
-            MoveTo(0, 0),
-            Print(style(&typed).with(Color::DarkGrey)),
-            SavePosition,
-            Print(test_word.char_at(0).unwrap()),
-        )?;
-
-        let (c, r) = cursor::position()?;
-        if c == 1 {
-            queue!(stdout(), MoveTo(0, r))?;
-        } else {
-            queue!(stdout(), RestorePosition)?;
-        }
-
-        queue!(
-            stdout(),
-            SavePosition,
-            Print(test_word.as_str()),
-            Print(style(remaining_words).with(Color::DarkGrey)),
-            RestorePosition,
-            Print(&test_word),
-        )?;
-
-        stdout().flush()?;
     }
 
     TestResults(finished_words)
 }
-
